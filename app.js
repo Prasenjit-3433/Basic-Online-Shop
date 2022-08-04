2db89ead84e1d0d28f5f5657138314d41f4c5efc
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2022-08-04 22:26:35 +0530

    resource-not-found middleware for all generic routes

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const checkAuthStatusMiddleware = require('./middlewares/check-auth');
 const protectRoutesMiddleware = require('./middlewares/protect-routes');
 const cartMiddleware = require('./middlewares/cart');
 const updateCartPricesMiddleware = require('./middlewares/update-cart-prices');
+const notFoundMiddleware = require('./middlewares/not-found');
 const authRoutes = require("./routes/auth.routes");
 const productsRoutes = require('./routes/products.routes');
 const baseRoutes = require('./routes/base.routes');
@@ -47,14 +48,16 @@ app.use(baseRoutes);
 app.use(authRoutes);
 app.use(productsRoutes);
 app.use('/cart', cartRoutes);
-app.use(protectRoutesMiddleware);
-app.use('/orders', ordersRoutes);
-app.use('/admin', adminRoutes);  
+app.use('/orders', protectRoutesMiddleware, ordersRoutes);
+app.use('/admin', protectRoutesMiddleware, adminRoutes); 
+
+app.use(notFoundMiddleware);
 
 // Note: only requests with a path that starts with '/admin' will make it into to the adminRoutes and this starting
 //       path '/admin' is then removed from the path of request. So that inside of the file adminRoutes, we can
 //       have the rest of the path..... 
 //       e.g: '/admin/products' --> 'products' ; '/admin/products/new' --> '/products/new'
+
 
 app.use(errorHandlerMiddleware);
 
